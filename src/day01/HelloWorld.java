83751e4a1d6543dc300757cc7bec330642a37d20
dilarakstk <169299986+dilarakstk@users.noreply.github.com>
2025-01-29 23:44:10 +0300

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,7 +3,15 @@ package day01;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello cydeo");
+         System.out.println("Hello programing");
+         System.out.println("Hello World");
+    
+    
+    
+    
+    
+    
     }
 
 
